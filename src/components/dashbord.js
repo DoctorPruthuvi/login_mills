88c874911d1a597c66b1038e93a9c4cf75068893
Your Name <deshan.amarathunga@gmail.com>
2020-05-27 21:57:06 +0530

    some changes in app.js

---
+++ src/components/dashbord.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+class Dashbord extends Component {
+    render() {
+        return (
+            <view><text>hi</text></view>
+        );
+    }
+}
+
+export default Dashbord;
